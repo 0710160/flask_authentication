d693d1114cd27eaf01d6b10ef4f675f77e0a228f
0710160 <xlvi@mm.st>
2021-10-27 14:15:50 +1300

    Almost done, just need to pass authentication status to templates

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Flask Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
+
+
+#Line below only required once, when creating DB.
 # db.create_all()
 
 
@@ -25,29 +37,76 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        password = generate_password_hash(
+            request.form["password"],
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        name = request.form["name"]
+        new_db_entry = User(
+            email=email,
+            password=password,
+            name=name
+        )
+        user = User.query.filter_by(email=email)
+        if user.query.filter_by(email=email).first():
+            flash("This username is already in use.")
+            return redirect(url_for('register'))
+        else:
+            db.session.add(new_db_entry)
+            db.session.commit()
+            return render_template("secrets.html", name=name)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        user = db.session.query(User).filter_by(email=email).first()
+        if not user:
+            flash("User does not exist in database. Please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, request.form["password"]):
+            flash("Incorrect password. Please try again.")
+            return redirect(url_for('login'))
+        else:
+            login_user(user, remember=False, duration=None, force=False, fresh=True)
+            return redirect(url_for('secrets'))
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    if not current_user.is_authenticated:
+        return app.login_manager.unauthorized()
+    else:
+        return render_template("secrets.html")
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
-@app.route('/download')
-def download():
-    pass
+@app.route('/download/<path:filename>')
+@login_required
+def download(filename):
+    app.config['PDF_DIR'] = 'static/files'
+    return send_from_directory(
+        directory=app.config['PDF_DIR'],
+        filename=filename,
+        as_attachment=True
+    )
 
 
 if __name__ == "__main__":
